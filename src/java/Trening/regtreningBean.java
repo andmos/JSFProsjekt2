075b0d7c67564202009a69dea136099a9260b5ba
Andreas <andreas.mosti@gmail.com>
2012-10-10 14:20:33 +0200

    lagt til treningsklasse og regBean

---
+++ src/java/Trening/regtreningBean.java
@@ -0,0 +1,25 @@
+
+package Trening;
+
+import java.io.Serializable; 
+import javax.inject.Inject; 
+import javax.enterprise.context.SessionScoped; 
+import java.util.ArrayList; 
+import javax.inject.Named;
+
+        
+        
+@Named ("reg") 
+
+public class regtreningBean implements Serializable {
+      TreningsOkt eiOkt = new TreningsOkt(); 
+      
+      public regtreningBean(){
+        
+      }
+      
+
+
+}
+
+
